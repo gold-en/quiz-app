27801bf492dcb8d9e20f4165a1042025807ef063
Golden1636 <okeamagolden@yahoo.com>
2023-02-16 10:35:27 +0100

    correct ans functional

---
+++ js/questions.js
@@ -0,0 +1,25 @@
+//creating an array and pasing the number, questions, options and answers
+let questions = [
+  {
+    numb: 1,
+    question: "what does HTML stand for?",
+    answer: "Hyper Text Markup Language",
+    options: [
+      "Hyper Text Processor",
+      "Hyper Text Markup Language",
+      "Hyper Text Multiple Language",
+      "Hyper Tool Multi Language",
+    ],
+  },
+  {
+    numb: 2,
+    question: "what does CSS stand for?",
+    answer: "Cascading Style Sheet",
+    options: [
+      "Common style sheet",
+      "Colorful Style Sheet",
+      "Computer Style Sheet",
+      "Cascading Style Sheet",
+    ],
+  },
+];
