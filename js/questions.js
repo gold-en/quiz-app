4ba92fbc658c7189fa78f7e754971b7b8a2e319a
Golden1636 <okeamagolden@yahoo.com>
2023-02-18 10:17:44 +0100

    15 questions added

--- js/questions.js
+++ js/questions.js
@@ -2,24 +2,150 @@
 let questions = [
   {
     numb: 1,
-    question: "what does HTML stand for?",
-    answer: "Hyper Text Markup Language",
-    options: [
-      "Hyper Text Processor",
-      "Hyper Text Markup Language",
-      "Hyper Text Multiple Language",
-      "Hyper Tool Multi Language",
-    ],
+    question: 'Inside which HTML element do we put the JavaScript?',
+    answer: '< script >',
+    options: ['< javascript >', '< scripting >', '< script >', '< jsScript >'],
   },
   {
     numb: 2,
-    question: "what does CSS stand for?",
-    answer: "Cascading Style Sheet",
+    question:
+      'What is the correct JavaScript syntax to change the content of the HTML element below? \n < p id="demo">This is a demonstration.< /p >',
+    answer: 'document.getElementById("demo").innerHTML="Hello World!";',
     options: [
-      "Common style sheet",
-      "Colorful Style Sheet",
-      "Computer Style Sheet",
-      "Cascading Style Sheet",
+      'document.getElementByName("p").innerHTML="Hello World!";',
+      'document.getElementById("demo").innerHTML="Hello World!";',
+      'document.getElement("p").innerHTML="Hello World!";',
+      '#demo.innerHTML="Hello World!";',
+    ],
+  },
+  {
+    numb: 3,
+    question: 'Where is the correct place to insert a JavaScript?',
+    answer: 'Both the < head > section and the < body > section are correct',
+    options: [
+      'The < body > section',
+      'Both the < head > section and the < body > section are correct',
+      'The < head > section',
+      'NOTA',
+    ],
+  },
+  {
+    numb: 4,
+    question:
+      'What is the correct syntax for referring to an external script called "xxx.js"?',
+    answer: '< script src="xxx.js" >',
+    options: [
+      '< script href="xxx.js" >',
+      '< script name="xxx.js >',
+      '< script src="xxx.js" >',
+      '< script src="jsfile" >',
+    ],
+  },
+  {
+    numb: 5,
+    question: 'The external JavaScript file must contain the < script > tag.',
+    answer: 'False',
+    options: ['True', 'False', 'AOTA', 'NOTA'],
+  },
+  {
+    numb: 6,
+    question: 'How do you write "Hello World" in an alert box?',
+    answer: 'alert("Hello World");',
+    options: [
+      'alert("Hello World");',
+      'msgBox("Hello World");',
+      'msg("Hello World");',
+      'alertBox("Hello World");',
+    ],
+  },
+  {
+    numb: 7,
+    question: 'How do you create a function in JavaScript?',
+    answer: 'function myFunction()',
+    options: [
+      'function:myFunction()',
+      'function myFunction()',
+      'function = myFunction()',
+      'jsfunction()',
+    ],
+  },
+  {
+    numb: 8,
+    question: 'How do you call a function named "myFunction"?',
+    answer: 'myFunction()',
+    options: [
+      'call function myFunction()',
+      'call myFunction()',
+      'myFunction()',
+      'this myFunction()',
+    ],
+  },
+  {
+    numb: 9,
+    question: 'How to write an IF statement in JavaScript?',
+    answer: 'if (i == 5)',
+    options: ['if i == 5 then', 'if i = 5', 'if (i == 5)', 'if i = 5 then'],
+  },
+  {
+    numb: 10,
+    question:
+      'How to write an IF statement for executing some code if "i" is NOT equal to 5?',
+    answer: 'if (i != 5)',
+    options: ['if i <> 5', 'if (i <> 5)', 'if i =! 5 then', 'if (i != 5)'],
+  },
+  {
+    numb: 11,
+    question: 'How does a WHILE loop start?',
+    answer: 'while (i <= 10)',
+    options: [
+      'while (i <= 10)',
+      'while (i <= 10; i++)',
+      'while i = 1 to 10',
+      'NOTA',
+    ],
+  },
+  {
+    numb: 12,
+    question: 'How does a FOR loop start?',
+    answer: 'for (i = 0; i <= 5; i++)',
+    options: [
+      'for (i = 0; i <= 5)',
+      'for (i = 0; i <= 5; i++)',
+      'for (i <= 5; i++)',
+      'for i = 1 to 5',
+    ],
+  },
+  {
+    numb: 13,
+    question: 'How can you add a comment in a JavaScript?',
+    answer: '//This is a comment',
+    options: [
+      "'This is a comment",
+      '//This is a comment',
+      '< !--This is a comment--> ',
+      'NOTA',
+    ],
+  },
+  {
+    numb: 14,
+    question: 'How to insert a comment that has more than one line?',
+    answer: '/*This comment has \n more than one line*/',
+    options: [
+      '< !--This comment has \n more than one line-- >',
+      '/*This comment has \n more than one line*/',
+      '//This comment has \n more than one line//',
+      'NOTA',
+    ],
+  },
+  {
+    numb: 15,
+    question: 'What is the correct way to write a JavaScript array?',
+    answer: 'var colors = ["red", "green", "blue"]',
+    options: [
+      'var colors = "red", "green", "blue"',
+      'var colors = ["red", "green", "blue"]',
+      'var colors = (1:"red", 2:"green", 3:"blue")',
+      'var colors = 1 = ("red"), 2 = ("green"), 3 = ("blue")',
     ],
   },
 ];
--- js/script.js
+++ js/script.js
@@ -7,8 +7,12 @@ const quiz_box = document.querySelector('.quiz_box');
 const next_btn = quiz_box.querySelector('.next_btn');
 const timeCount = quiz_box.querySelector('.timer .timer_sec');
 const timeLine = quiz_box.querySelector('header .time_line');
+const timeOff = quiz_box.querySelector('header .time_text');
 const option_list = document.querySelector('.option_list');
 const option = option_list.querySelectorAll('.option');
+const result_box = document.querySelector('.result_box');
+const restart_quiz = result_box.querySelector('.buttons .restart');
+const quit_quiz = result_box.querySelector('.buttons .quit');
 
 //If Start Quiz Button Clicked
 start_btn.onclick = () => {
@@ -30,11 +34,17 @@ continue_btn.onclick = () => {
   startTimerLine(0);
 };
 
+//if the Quit Quiz Button is clicked
+quit_quiz.onclick = () => {
+  window.location.reload();
+};
+
 let que_count = 0;
 let que_numb = 1;
 let counter, counterLine;
 let timeValue = 15;
 let widthValue = 0;
+let userScore = 0;
 
 //If Next Button Clicked
 next_btn.onclick = () => {
@@ -48,8 +58,12 @@ next_btn.onclick = () => {
     clearInterval(counterLine);
     startTimerLine(widthValue);
     next_btn.style.display = 'none';
+    timeOff.textContent = 'Time Left';
   } else {
+    clearInterval(counter);
+    clearInterval(counterLine);
     console.log('Questions completed');
+    showResultbox();
   }
 };
 
@@ -90,6 +104,8 @@ const optionSelected = answer => {
   let correctAns = questions[que_count].answer.trim();
   let allOptions = option_list.children.length;
   if (userAns == correctAns) {
+    userScore += 1;
+    console.log(userScore);
     answer.classList.add('correct');
     console.log(answer);
     answer.insertAdjacentHTML('beforeend', tickIcon);
@@ -114,6 +130,23 @@ const optionSelected = answer => {
   next_btn.style.display = 'block';
 };
 
+const showResultbox = function () {
+  info_box.classList.remove('activeInfo'); //hides the info box
+  quiz_box.classList.remove('activeQuiz'); //hides the quiz box
+  result_box.classList.add('activeResult'); //shows the result box
+  const scoreText = result_box.querySelector('.score_text');
+  if (userScore > 3) {
+    let scoreTag = `<span>and congrats!, you got <p>${userScore}</p>out of<p>${questions.length}</p></span>`;
+    scoreText.innerHTML = scoreTag;
+  } else if (userScore > 1) {
+    let scoreTag = `<span>and nice, you got <p>${userScore}</p>out of<p>${questions.length}</p></span>`;
+    scoreText.innerHTML = scoreTag;
+  } else {
+    let scoreTag = `<span>and sorry, you got only<p>${userScore}</p>out of<p>${questions.length}</p></span>`;
+    scoreText.innerHTML = scoreTag;
+  }
+};
+
 const startTimer = function (time) {
   const timer = () => {
     timeCount.textContent = time;
@@ -125,6 +158,21 @@ const startTimer = function (time) {
     if (time < 0) {
       clearInterval(counter);
       timeCount.textContent = '00';
+      timeOff.textContent = 'Time Off';
+
+      let correctAns = questions[que_count].answer.trim();
+      let allOptions = option_list.children.length;
+
+      for (let i = 0; i < allOptions; i++) {
+        if (option_list.children[i].textContent.trim() === correctAns) {
+          option_list.children[i].setAttribute('class', 'option correct');
+          option_list.children[i].insertAdjacentHTML('beforeend', tickIcon);
+        }
+      }
+      for (let i = 0; i < allOptions; i++) {
+        option_list.children[i].classList.add('disabled');
+      }
+      next_btn.style.display = 'block';
     }
   };
   counter = setInterval(timer, 1000);
@@ -143,6 +191,24 @@ const startTimerLine = function (time) {
 
 const queCounter = index => {
   const bottom_ques_counter = quiz_box.querySelector('.total_que');
-  let totalQuesCountTag = `<span><p>${que_numb}</p>of<p>${questions.length}</p>Questions</span>`;
+  let totalQuesCountTag = `<span><p>${index}</p>of<p>${questions.length}</p>Questions</span>`;
   bottom_ques_counter.innerHTML = totalQuesCountTag;
+};
+
+//if the Replay Quiz button is clicked
+restart_quiz.onclick = () => {
+  quiz_box.classList.add('activeQuiz');
+  result_box.classList.remove('activeResult');
+  que_count = 0;
+  que_numb = 1;
+  timeValue = 15;
+  widthValue = 0;
+  userScore = 0;
+  showQuestions(que_count);
+  queCounter(que_numb);
+  clearInterval(counter);
+  startTimer(timeValue);
+  clearInterval(counterLine);
+  startTimerLine(widthValue);
+  next_btn.style.display = 'none';
 };
